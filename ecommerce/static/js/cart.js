97cee92936af788fb37148bed76f0c598a0c3802
Cristia Solis <cri.solis@duocuc.cl>
2024-07-17 19:38:19 -0400

    et

---
+++ ecommerce/static/js/cart.js
@@ -0,0 +1,44 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:', user)
+        if (user === 'AnonymousUser'){
+            console.log('Usuario no identificado')
+        } else {
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('Usuario identificado, enviando información...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log('Data:', data)
+        location.reload()
+    })
+    .catch((error) => {
+        console.error('Error:', error)
+    });
+}
